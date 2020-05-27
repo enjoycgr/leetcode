34817a103b1b07a0b9c8995d4da92302a72bb3d5
chenguorong <464884015@qq.com>
2020-05-27 18:00:26 +0800

    优化64

--- 64/64.go
+++ 64/64.go
@@ -26,39 +26,31 @@ import "fmt"
 func minPathSum(grid [][]int) int {
 	var m = len(grid)
 	var n = len(grid[0])
-
-	var dp = make([][]int, m)
-
-	for i := 0; i < m; i++ {
-		dp[i] = make([]int, n)
-	}
-
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
 			if i == 0 && j == 0 {
-				dp[i][j] = grid[i][j]
 				continue
 			}
 
-			if i == 0 {
-				dp[i][j] = grid[i][j] + dp[i][j-1]
+			if i == 0 && j != 0 {
+				grid[i][j] = grid[i][j] + grid[i][j-1]
 				continue
 			}
 
-			if j == 0 {
-				dp[i][j] = grid[i][j] + dp[i-1][j]
+			if j == 0 && i != 0 {
+				grid[i][j] = grid[i][j] + grid[i-1][j]
 				continue
 			}
 
-			if dp[i][j-1] < dp[i-1][j] {
-				dp[i][j] = dp[i][j-1] + grid[i][j]
+			if grid[i][j-1] < grid[i-1][j] {
+				grid[i][j] = grid[i][j-1] + grid[i][j]
 			} else {
-				dp[i][j] = dp[i-1][j] + grid[i][j]
+				grid[i][j] = grid[i-1][j] + grid[i][j]
 			}
 		}
 	}
 
-	return dp[m-1][n-1]
+	return grid[m-1][n-1]
 }
 
 func main() {
